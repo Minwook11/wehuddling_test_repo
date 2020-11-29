15c24809ee83460ccfa68bb797729b63aad409e2
Minwook11 <alsdnr4874@gmail.com>
2020-11-29 20:25:09 +0900

    Add : Shipper App API 작성 - 배송자에게 할당된 주문 확인 API - 배송을 완료했음을 체크하기 위한 API

--- shipper/urls.py
+++ shipper/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import ShipperView
+from .views import ShipperView, OrderView
 
 urlpatterns = [
     path('', ShipperView.as_view()),
+    path('/<int:shipper_id>', OrderView.as_view()),
+    path('/check', OrderView.as_view()),
 ]
--- shipper/views.py
+++ shipper/views.py
@@ -4,6 +4,7 @@ from django.views import View
 from django.http import JsonResponse
 
 from .models import Shipper
+from customer.models import Order
 
 class ShipperView(View):
     def post(self, request):
@@ -23,6 +24,47 @@ class ShipperView(View):
                 phone_number = input_phone_number,
                 region = input_region
             ).save()
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+        return JsonResponse({'message' : 'SUCCESS'}, status = 200)
+
+class OrderView(View):
+    def get(self, request, shipper_id):
+        shipper = Shipper.objects.get(id = shipper_id)
+        if not(shipper):
+            return JsonResponse({'message' : 'WRONG_SHIPPER_ID'}, status = 400)
+
+        check_orders = Order.objects.filter(shipper_id = shipper_id)
+        if not(check_orders):
+            return JsonResponse({'mseaage' : 'NO_ORDER'}, status = 200)
+
+        order_list = [{
+            'order_id' : order.id,
+            'address' : order.address,
+            'quantity' : order.quantity,
+        } for order in check_orders]
+        return JsonResponse({'message' : order_list}, status = 200)
+
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            target_order_id = data['order_id']
+            target_shipper_id = data['shipper_id']
+
+            if not(Order.objects.filter(id = target_order_id).exists()):
+                return JsonResponse({'message' : 'WRONG_ORDER_ID'}, status = 400)
+
+            if not(Shipper.objects.filter(id = target_shipper_id).exists()):
+                return JsonResponse({'message' : 'WRONG_SHIPPER_ID'}, status = 400)
+
+            if not(Order.objects.filter(id = target_order_id, shipper_id = target_shipper_id).exists()):
+                return JsonResponse({'message' : 'WRONG_ORDER_SHIPPER_ID_CONNECT'}, status = 400)
+
+            target_order = Order.objects.get(id = target_order_id)
+            if target_order.status == 2:
+                return JsonResponse({'message' : 'ORDER_ALREADY_FINISHED'}, status = 400)
+            target_order.status = 2
+            target_order.save()
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
         return JsonResponse({'message' : 'SUCCESS'}, status = 200)
